cf5c7289edbd7a7e8dcb88ea7b56c8133ec18d28
Gabriela Gonçalves <hi.gabidigital@gmail.com>
2023-05-30 20:42:45 -0300

    First commit

---
+++ app/routes/posts.admin.$slug.tsx
@@ -0,0 +1,115 @@
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react";
+import invariant from "tiny-invariant";
+
+import { getPost, updatePost } from "~/models/post.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  return json({ post });
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  // TODO: remove me
+  await new Promise((res) => setTimeout(res, 1000));
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
+  if (hasErrors) {
+    return json(errors);
+  }
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
+
+  await updatePost(slug, { title, slug, markdown });
+
+  return redirect("/posts/admin");
+};
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
+export default function PostSlug() {
+  const errors = useActionData<typeof action>();
+  const { post } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const isCreating = Boolean(navigation.state === "submitting");
+
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
+          <input
+            type="text"
+            name="title"
+            className={inputClassName}
+            defaultValue={post.title}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
+          <input
+            type="text"
+            name="slug"
+            className={inputClassName}
+            defaultValue={post.slug}
+          />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">
+          Markdown:{" "}
+          {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}{" "}
+        </label>
+        <br />
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+          defaultValue={post.markdown}
+        />
+      </p>
+      <p className="text-right">
+        <button
+          type="submit"
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
+        >
+          {isCreating ? "Creating..." : "Create Post"}
+        </button>
+      </p>
+    </Form>
+  );
+}
